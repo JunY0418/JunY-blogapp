2b978403b073fdef953c5e8d0cc4c36c114526ea
JunY <make.the.best.of.destiny@gmail.com>
2023-03-25 16:47:50 +0900

    ArticleControllerに書き換え

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,8 @@
-class HomeController < ApplicationController
+class ArticlesController < ApplicationController
     # 自動読み込みを有効にするための行
     require_dependency 'article'
     def index
       @article = Article.first
     end
 
-    def about
-
-    end
 end
