2ce7e1bc4ff083bff8a0f7d97598561c499f7f8f
JunY <make.the.best.of.destiny@gmail.com>
2023-03-26 11:01:20 +0900

    訂正しました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,10 @@ class ArticlesController < ApplicationController
     # 自動読み込みを有効にするための行
     require_dependency 'article'
     def index
-      @article = Article.first
+      @articles = Article.all
+    end
+    def show
+      @article = Article.find(params[:id])
     end
 
 end
