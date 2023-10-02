ae62f92468a5d1e72cb0d010d44f54886988d1d1
Jun <jun.liberal418@gmail.com>
2023-10-02 19:13:56 +0900

    CSSが反応するように修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,2 @@
-//= link heart.svg
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-
-
-//= link application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
-//= link controllers/application.js
