dc132554b22382e7c93741404f021f194af703d6
Jun <jun.liberal418@gmail.com>
2023-10-02 23:27:58 +0900

    Revert "コンテント追加"
    This reverts commit d1eba396e463a51cdc471e9f6d1fa29767673147.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,8 @@
+//= link heart.svg
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+
 //= link application.js
 //= link controllers/hello_controller.js
 //= link controllers/index.js
