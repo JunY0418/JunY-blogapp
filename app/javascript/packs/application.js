968a573611be54f18f0938d7c02b3c77c6d2c37d
Jun <jun.liberal418@gmail.com>
2023-12-28 17:09:59 +0900

    RubyではなくてJavaScriptによって「いいね」の状態を切り替えられるよう

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,11 +20,35 @@ Rails.start();
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+}
+
 document.addEventListener('turbolinks:load', () => {
-  const dataset = $(`#article-show`).data()
+  const dataset = $('#article-show').data()
   const articleId = dataset.articleId
-  axios.get(`/articles/1/like`)
-  .then((response) =>
-    console.log(response)
-  )
+
+  axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      const hasLiked = response.data.hasLiked
+      handleHeartDisplay(hasLiked)
+    })
 })
+
+// document.addEventListener('turbolinks:load', () => {
+//   const dataset = $('#article-show').data()
+//   const articleId = dataset.articleId
+//   axios.get(`/articles/${articleId}/like`)
+//     .then((response) => {
+//       const hasLiked = response.data.hasLiked
+//       if (hasLiked) {
+//         $('.active-heart').removeClass('hidden')
+//       } else {
+//         $('.inactive-heart').removeClass('hidden')
+//       }
+//     })
+// })
