dc80fd3340638df1c95f18c8bc2fffc074aabb95
Jun <jun.liberal418@gmail.com>
2024-03-27 18:54:12 +0900

    02. メールを送付

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -1,14 +1,7 @@
-class RealtionshipMailer < ApplicationMailer
+class RelationshipMailer < ApplicationMailer
   def new_follower(user, follower)
     @user = user
     @follower = follower
     mail to: user.email, subject: '【お知らせ】フォローされました'
-
-  end
-
-  def welcome
   end
 end
-
-
-RealtionshipMailer.new_follower(User.first)
