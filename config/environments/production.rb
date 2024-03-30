82bd8f154b5c26eb28594f524a3b8f53eb454457
Jun <jun.liberal418@gmail.com>
2024-03-30 16:04:57 +0900

    mailgunの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 送信者のメールアドレスのドメイン
+  config.action_mailer.default_url_options = { host: 'jun.liberal418@gmail.com' }
+
+  # Mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['HEROKU_DOMAIN'],
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
